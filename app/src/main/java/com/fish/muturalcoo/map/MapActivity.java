df5081977a2de0473fe64b0d3ba6742141938f22
Allen <lejunshu@gmail.com>
2018-05-31 23:52:02 +0800

    feature: 继承百度定位

--- app/src/main/java/com/fish/muturalcoo/map/MapActivity.java
+++ app/src/main/java/com/fish/muturalcoo/map/MapActivity.java
@@ -6,6 +6,14 @@ import android.widget.TextView;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.fish.muturalcoo.R;
 import com.fish.muturalcoo.base.BaseActivity;
 
@@ -17,15 +25,22 @@ public class MapActivity extends BaseActivity {
 
     @Bind(R.id.tv_position)
     TextView tvPosition;
+
+    @Bind(R.id.bmapView)
+    MapView bmapView;
     private LocationClient locationClient;
+
+    private boolean isFirstLocate = true;
+    private BaiduMap baiduMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initData();
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
 
-        initData();
         initView();
         initListener();
     }
@@ -34,10 +49,15 @@ public class MapActivity extends BaseActivity {
     public void initData() {
         locationClient = new LocationClient(getApplicationContext());
         locationClient.registerLocationListener(new MyLocationListener());
+
+        SDKInitializer.initialize(getApplicationContext());
+
     }
 
     @Override
     public void initView() {
+        baiduMap = bmapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
         requestLocation();
 
     }
@@ -46,7 +66,16 @@ public class MapActivity extends BaseActivity {
      * 请求位置信息
      */
     private void requestLocation() {
+        iniLocation();
         locationClient.start();
+    }
+
+    private void iniLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
+//        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        locationClient.setLocOption(option);
     }
 
     @Override
@@ -58,9 +87,19 @@ public class MapActivity extends BaseActivity {
 
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation || bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
+                navigateTo(bdLocation);
+
+            }
+
             StringBuilder currentPosition = new StringBuilder();
             currentPosition.append("纬度：").append(bdLocation.getLatitude()).append("\n");
             currentPosition.append("经度：").append(bdLocation.getLongitude()).append("\n");
+            currentPosition.append("国家：").append(bdLocation.getCountry()).append("\n");
+            currentPosition.append("省：").append(bdLocation.getProvince()).append("\n");
+            currentPosition.append("市：").append(bdLocation.getCity()).append("\n");
+            currentPosition.append("区：").append(bdLocation.getDistrict()).append("\n");
+            currentPosition.append("街道：").append(bdLocation.getStreet()).append("\n");
             currentPosition.append("定位方式：");
             if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
                 currentPosition.append("网络");
@@ -73,4 +112,44 @@ public class MapActivity extends BaseActivity {
 
 
     }
+
+    private void navigateTo(BDLocation bdLocation) {
+        if (isFirstLocate) {
+            LatLng ll = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            isFirstLocate = false;
+        }
+
+        MyLocationData.Builder builder=new MyLocationData.Builder();
+        builder.latitude(bdLocation.getLatitude());
+        builder.longitude(bdLocation.getLongitude());
+        MyLocationData locationData=builder.build();
+        baiduMap.setMyLocationData(locationData);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationClient.stop();
+        bmapView.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bmapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        bmapView.onPause();
+    }
+
+
 }
